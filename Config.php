8132c5e01e3c97c24447cf9dfe8669a17960dd19
LuPe_BReak <lfbmrj15@gmail.com>
2019-02-25 14:54:58 -0300

    Update config

--- Config.php
+++ Config.php
@@ -1,10 +1,14 @@
 <?php
-//configuraçoes para o acesso ao banco de dados
+//Configuraçoes para o acesso ao banco de dados
 $host="localhost";
 $user="root";
 $pass="";
 $db="etapa2";
+
+//variavel para identificar o link do host ( nao modifique impensadamente)
 $root = $_SERVER['HTTP_HOST'];
+
+//Configuraçoes para o recaptcha, instruçoes encontradas no readme na area de instalaçao
 $recaptcha_secret="Your_secre_key_here";
 $recaptcha_key="Your_key_here";
 ?>
